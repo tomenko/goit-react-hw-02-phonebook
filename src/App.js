8da2e2899fca96898f98f251f983bd1b21106ab5
tomenko <tomenko.sl@gmail.com>
2021-04-27 22:19:30 +0300

    ошибки исправлены

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Filter from './components/Filter'
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -41,23 +46,34 @@ class App extends Component {
   };
 
   
-  getCurrentContacts = () => {
+/*   getCurrentContacts = () => {
     const { contacts, filter } = this.state;
     const normalFilter = filter.toLowerCase();
 
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalFilter),
     );
+  }; */
+
+  getCurrentContacts = () => {
+    const { filter, contacts } = this.state;
+    const regExp = new RegExp(filter, 'gi');
+
+    if (filter) {
+      return contacts.filter(contact => regExp.test(contact.name));
+    }
+    return contacts;
   };
 
   render() {
+      console.log(this.getCurrentContacts());
       return (
       <>
         <h1>Phonebook</h1>
         <ContactForm onSubmitForm = {this.formSubmitHandler} />
         <h2>Contacts</h2>
         <Filter id={shortid.generate()} hendleInputFilter = {this.hendleInputFilter} />
-        <ContactList contacts={this.getCurrentContacts}  onDeleteContact={this.deleteContact}/>
+        <ContactList contacts={this.getCurrentContacts()}  onDeleteContact={this.deleteContact}/>
       </>
     )
   }
